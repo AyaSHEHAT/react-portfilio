559b20c9395c40e53c7a3f0248164b02b8eb2013
AyaSHEHAT <ayashehata274@yahoo.com>
2022-11-09 00:04:26 +0200

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import  'bootstrap/dist/css/bootstrap.min.css';
-
 import NavBar from './Components/NavBar';
+import Header from './Components/Header';
+
 
 function App() {
   return (
     <div>
+    
       <NavBar />
-    <h1>portfolio project.......</h1>
+      <Header/>
+
     </div>
     
 
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <div className='header-wraper'>
+        <div className='main-info'>
+            <h1>web development and websites promotions</h1>
+           
+            <a href='#' className='btn-main-offer'>Contact Me</a>
+         </div>
+    </div>
+  )
+}
+
+export default Header
